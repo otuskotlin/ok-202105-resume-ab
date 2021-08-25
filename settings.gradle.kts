172cbf6517f15d1438cd20e2b6c9922c0f07cfdd
АлексейБулаев <assass1n@bk.ru>
2021-08-25 10:34:16 +0300

    m3-api-init -> Создать подпроект в Gradle "*-transport-main-openapi", в котором будут размещаться модели API с использованием технологии OpenAPI ->

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,10 +3,15 @@ rootProject.name = "resume-ab"
 pluginManagement {
     plugins {
         val kotlinVersion: String by settings
+        val openApiVersion: String by settings
         kotlin("jvm") version kotlinVersion
         kotlin("multiplatform") version kotlinVersion
+        kotlin("plugin.serialization") version kotlinVersion
+        id("org.openapi.generator") version openApiVersion
     }
 }
 
 include("common-kmp")
 include("common")
+include("resume-ab-transport-main-openapi")
+include("resume-ab-transport-main-mp")
